10d835b271d32b2a541b950aad8c26ee7edc66b3
joh66888 <joh66888@gmail.com>
2026-01-05 11:20:10 +0800

    Renew the material

--- src/try.js
+++ src/try.js
@@ -140,7 +140,7 @@ scene.add(spaceshipModel);
 
 // Textures 太空船材質
 const textLoader = new THREE.TextureLoader();
-const metcapTexture = textLoader.load('./textures/matcaps/14.png');
+const metcapTexture = textLoader.load('./textures/matcaps/12.png');
 metcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 // Material
